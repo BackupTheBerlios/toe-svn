81a77cdb6441b9683b11e1c85559bc33368bff25
dannym <dannym@97d5a352-4916-0410-88e1-e444b420151b>
2006-06-24 19:32:45 +0000

    add ARM

--- trunk/allocator/registerallocator.py
+++ trunk/allocator/registerallocator.py
@@ -20,8 +20,14 @@ from cpuallocator import TRegisterId
 
 class TStackRegister(cpuallocator.TRegister):
 	def __init__(self, offset):
-		cpuallocator.TRegister.__init__(self)
+		cpuallocator.TRegister.__init__(self, TRegisterId(128 + offset))
 		self._offset = offset
+
+	def __str__(self):
+		return "in stack frame at %d" % self._offset
+
+	def __repr__(self):
+		return str(self)
 
 	offset = property(lambda self: self._offset)
 
@@ -34,6 +40,7 @@ class TCustomRegisterAllocator(object):
 	def __init__(self, cpu):
 		self._cpu = cpu
 		self._stack_allocator = self.__class__.stack_allocator_class()
+		self._stack_offsets = [] # keep sorted.
 
 	# returns the allocated register or throws exception.
 	def allocate(self, guest, preferred_id = TRegisterId.Any, is_stack_ok = True):
@@ -42,7 +49,22 @@ class TCustomRegisterAllocator(object):
 			if is_stack_ok and self._stack_allocator != None:
 				offset = self._stack_allocator.push()
 				# TODO
-			
+
+				i = 0
+				for i in range(len(self._stack_offsets)):
+					if self._stack_offsets[i][0] > offset:
+						break
+
+				register = TStackRegister(offset)
+				self._stack_offsets.insert(i, (offset, register))
+
+				t = self._stack_offsets[:]
+				t.sort()
+				assert(t == self._stack_offsets)
+				return register
+		else:
+			return register
+
 		raise ERegisterUnavailable("E2006062417: no register available")
 
 	def clobber(self, id):
@@ -62,15 +84,33 @@ class TX86RegisterAllocator(TCustomRegisterAllocator):
 	>>> cpu = cpuallocator.TX86CPU()
 	>>> registers = TX86RegisterAllocator(cpu)
 	>>> registers.print_state()
-	0: None
-	1: None
-	2: None
-	3: None
-	4: None
-	5: None
-	6: None
-	7: None
-	8: None
+	eax: None
+	ebx: None
+	ecx: None
+	edx: None
+	esi: None
+	edi: None
+	esp: Illuminati
+	ebp: Illuminati
+	eip: Illuminati
+	>>> registers.allocate(1)
+	eax
+	>>> registers.allocate(2)
+	ebx
+	>>> registers.allocate(7)
+	ecx
+	>>> registers.allocate(3)
+	edx
+	>>> registers.allocate(100)
+	esi
+	>>> registers.allocate(200)
+	edi
+	>>> registers.allocate(300)
+	in stack frame at 0
+	>>> registers.allocate(400)
+	in stack frame at 4
+	>>> registers.allocate(4)
+	in stack frame at 8
 	"""
 	stack_allocator_class = stackallocator.TX86StackAllocator
 
@@ -83,6 +123,59 @@ to the stack for everything.
 """
 class TX87RegisterAllocator(TCustomRegisterAllocator):
 	stack_allocator_class = stackallocator.TX87StackAllocator
+
+	def __init__(self, cpu):
+		TCustomRegisterAllocator.__init__(self, cpu)
+
+class TARMRegisterAllocator(TCustomRegisterAllocator):
+	"""
+	>>> cpu = cpuallocator.TARMCPU()
+	>>> registers = TARMRegisterAllocator(cpu)
+	>>> registers.print_state()
+	r0: None
+	r1: None
+	r2: None
+	r3: None
+	r4: None
+	r5: None
+	r6: None
+	r7: None
+	r8: None
+	r9: None
+	Stack_Limit: Illuminati
+	Frame_Pointer: Illuminati
+	I_Pointer: Illuminati
+	Stack_Pointer: Illuminati
+	Link_Return: Illuminati
+	Program_Counter: Illuminati
+	>>> registers.allocate(1)
+	r0
+	>>> registers.allocate(2)
+	r1
+	>>> registers.allocate(7)
+	r2
+	>>> registers.allocate(3)
+	r3
+	>>> registers.allocate(100)
+	r4
+	>>> registers.allocate(200)
+	r5
+	>>> registers.allocate(201)
+	r6
+	>>> registers.allocate(202)
+	r7
+	>>> registers.allocate(203)
+	r8
+	>>> registers.allocate(204)
+	r9
+	>>> registers.allocate(300)
+	in stack frame at 0
+	>>> registers.allocate(400)
+	in stack frame at 4
+	>>> registers.allocate(4)
+	in stack frame at 8
+	"""
+	stack_allocator_class = stackallocator.TX86StackAllocator
 
 	def __init__(self, cpu):
 		TCustomRegisterAllocator.__init__(self, cpu)
